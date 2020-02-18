c1effa59fa598bdf97c3d0af8615822c9a25d658
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-02-18 15:50:50 -0300

    df

--- resources/views/paginas/paginas_personalizadas/fabian_pagina.blade.php
+++ resources/views/paginas/paginas_personalizadas/fabian_pagina.blade.php
@@ -210,7 +210,7 @@
                  <img src="{{$ImagenParaTaG}}" class="img-atencion-precios">
                  <a href="#contact-section" class="btn smoothscroll btn-primary"> Contáctanos</a>
                  <img class="icono-uruguay-home" src="{{url()}}/imagenes/BikeUruguay/Bike-Uruguay-Tours-bandera-uruguay.png" alt="BikingUruguay bandera de Uruguay. Hacemos Bici- Tours guiados en Montevideo y Punta Del Este">
-                 <img class="logo-float-easy-socio-portada" src="{{url()}}/imagenes/paginas_personalizadas/BikeUruguay/logo-bike-uruguay-color-blanco-pgxperience.png" alt="Bikin Uruguay logo. Pg-Xperience logo. Toutrs en bici logo.">
+                 <img class="logo-float-easy-socio-portada" src="{{url()}}/imagenes/BikeUruguay/logo-bike-uruguay-color-blanco-pgxperience.png" alt="Biking Uruguay logo. Pg-Xperience logo. Toutrs en bici logo.">
                 </div>
           
         </div>
