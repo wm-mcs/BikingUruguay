e880081591e5087472b69f1ae14d7fa3212fadb2
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-05 13:00:47 -0300

    df

--- resources/views/layouts/paginasPersonalizadas/layout.blade.php
+++ resources/views/layouts/paginasPersonalizadas/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="{{ asset('imagenes/favicon.ico') }}"> 
     
-    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;700&family=Work+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="fonts/icomoon/style.css">
     <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">  
