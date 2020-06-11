5cb77225eba0eecb39cc958763bb48b95e94e754
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-11 14:22:38 -0300

    fg

--- resources/views/paginas/paginas_personalizadas/home_que_hacemos.blade.php
+++ resources/views/paginas/paginas_personalizadas/home_que_hacemos.blade.php
@@ -1,24 +1,24 @@
      <div class="site-section background-white" id="que_hacemos">
-        <div class="container">
-          
-          <h3 class="sub-titulos-class text-center text-color-primary font-italic mb-3">
-            Somos la extensión de actividades que necesita tu empresa de turismo.
-          </h3>    
+        <div class="container">          
+          <div class="row justify-content-center">          
+             <h3 class="sub-titulos-class text-center text-color-primary  mb-5">
+                Somos la extensión de actividades que necesita tu empresa de turismo.
+             </h3>    
+             <div class="col-10 pl-4">
+                <p class="lista-class mb-3">
+                   Conjugamos deporte, placer y turismo, formando una ecuación perfecta, destinada al área de servicios turísticos.
+                </p> 
 
+                <p class="lista-class mb-3"> 
+                   <strong>Programamos tours</strong>  por la Rambla de Montevideo y Punta del Este con <strong>guías bilingües y trilingües</strong> , tocando puntos históricos y bellos paisajes, en los que se puedan tomar los mejores registros fotográficos de estos lugares.
+                </p> 
 
-         
+                <p class="lista-class mb-3">
+                  También tenemos <strong>programas especiales para congresos y ejecutivos que vienen del exterior</strong>  que quieran realizar una dinámica grupal y diferente para conocer Montevideo y Punta del Este.
+                </p>
+             </div> 
 
-          <p class="text-center">
-             Conjugamos deporte, placer y turismo, formando una ecuación perfecta, destinada al área de servicios turísticos.
-          </p> 
-
-          <p class="text-center"> 
-             <strong>Programamos tours</strong>  por la Rambla de Montevideo y Punta del Este con <strong>guías bilingües y trilingües</strong> , tocando puntos históricos y bellos paisajes, en los que se puedan tomar los mejores registros fotográficos de estos lugares.
-          </p> 
-
-          <p class="text-center">
-            También tenemos <strong>programas especiales para congresos y ejecutivos que vienen del exterior</strong>  que quieran realizar una dinámica grupal y diferente para conocer Montevideo y Punta del Este.
-          </p>
-          
+                          
+          </div> 
         </div>
       </div>
