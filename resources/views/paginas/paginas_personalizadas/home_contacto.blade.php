f5a83fb6d91700f7ed5fd7c661f9702d7216ae39
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-11 15:12:18 -0300

    df

--- resources/views/paginas/paginas_personalizadas/home_contacto.blade.php
+++ resources/views/paginas/paginas_personalizadas/home_contacto.blade.php
@@ -7,7 +7,7 @@
             <h3 class="col-12 titulos-class text-color-primary mb-4 text-center">Contáctate ahora</h3>
             <p class="col-12 text-color-primary parrafo-class mb-3 text-center">Para contactarte rellenar el formulario de aquí abajo o llamá al <i class="fas fa-mobile-alt"></i> @{{empresa.celular}}.  </p>
 
-            <div class="col-6 col-lg-5 p-5 mb-2">
+            <div class="col-6 col-lg-5 p-1 p-lg-5 mb-2">
              <img src="{{url()}}/imagenes/team/paolo-biking-uruguay-perfil-atencion.jpg" class="img-fluid p5  rounded-circle ">
              
             </div>
