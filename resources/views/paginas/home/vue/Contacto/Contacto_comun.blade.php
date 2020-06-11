9d318fbf4e4e607d9b4194cbc9a596deba1e5165
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-06-11 13:47:17 -0300

    gh

--- resources/views/paginas/home/vue/Contacto/Contacto_comun.blade.php
+++ resources/views/paginas/home/vue/Contacto/Contacto_comun.blade.php
@@ -12,11 +12,40 @@
       </div>
     </div>
 
+
+
+    <div class="row mb-4 justify-content-end">
+
+      <p class="col-12 text-bold -text-primary mb-4">¿Qué necesitas?</p>
+
+      <div class="row align-items-center  col-12 mb-2">
+        <input class="m-0 mr-2" type="checkbox" name="" value="Programar un tour" v-model="data_mensaje.que_necesitas">
+        <p class="color-text-gris m-0">Programar un tour</p>
+      </div>
+      <div class="row align-items-center  col-12 mb-2">
+        <input class="m-0 mr-2" type="checkbox" name="" value="Más actividades para mi hotel" v-model="data_mensaje.que_necesitas">
+        <p class="color-text-gris m-0">Más actividades para mi hotel</p>
+      </div>
+       <div class="row align-items-center  col-12 mb-2">
+        <input class="m-0 mr-2" type="checkbox" name="" value="Bici tour para crucero" v-model="data_mensaje.que_necesitas">
+        <p class="color-text-gris m-0">Bici tour para crucero</p>
+      </div>
+      <div class="row align-items-center  col-12 mb-2">
+        <input class="m-0 mr-2" type="checkbox" name="" value="Otras actividades" v-model="data_mensaje.que_necesitas">
+        <p class="color-text-gris m-0">Otras actividades</p>
+      </div>
+      
+     
+      
+      
+    </div>
+
+
    
    
     <div class="row mb-4">
       <div class="form-group col-12">
-        <textarea v-model="data_mensaje.mensaje" cols="30" rows="2" :class="classImput" placeholder="Mensaje"></textarea>
+        <textarea v-model="data_mensaje.mensaje" cols="30" rows="2" :class="classImput" placeholder="Explica de manera detallada qué necesitas"></textarea>
       </div>
     </div>
 
@@ -48,6 +77,7 @@
     
   </div>
 </div>
-<div v-else class="section-title text-centermb-5" :class="classTextColor">
-  <h2 class="section-title text-center text-white mb-5">@{{mensaje_se_envio}}</h2>
+<div v-else class="section-title text-center mb-5" :class="classTextColor">
+  <h2 class="section-title text-center mb-5" :class="classTextColor" >@{{mensaje_se_envio}}</h2>
+  <a href="{{route('get_home')}}" class="Boton-Fuente-Chica Boton-Primario-Relleno mb-4 mt-4">Seguir explorando servicios </a>
 </div>
